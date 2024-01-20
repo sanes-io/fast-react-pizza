0ccfa5b3f46e34db3e78fedb77b8b06325cb299b
Sander Neset <sander.neset.dev@gmail.com>
2024-01-20 13:05:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fast React Pizza
+
+## Pizza ordering app for fictional restaurant "Fast React Pizza".
+
+## Built with React, Vite and Redux Toolkit.
---
+++ prettier.config.cjs
@@ -0,0 +1,7 @@
+// prettier.config.js
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  singleQuote: true,
+  printWidth: 80,
+};
+module.exports = config;
